fc5c0ba58efee4ef04410f249f6ee3b4bdefc6e9
Gaurav Munjal <gaurav.munjal@civicplus.com>
2026-03-30 19:44:24 -0400

    Replace fresh-tape with my fork

--- examples/webpack/tests/hello_test.js
+++ examples/webpack/tests/hello_test.js
@@ -1,4 +1,4 @@
-var test = window.tape = require('fresh-tape');
+var test = window.tape = require('@gaurav9576/fresh-tape');
 test('constructor', async function(t) {
     await new Promise(function(resolve) { setTimeout(resolve, 500); });
     t.pass('yahoo');
